9b76046aedd7cbe772739b8e6d0b71fa419d7191
Matteo Vaccari <mvaccari@thoughtworks.com>
2025-09-17 17:34:39 +0200

    fmt

--- cmd/wc/main.go
+++ cmd/wc/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "os"
+	"os"
 
-    "wc/internal/cli"
-    "wc/internal/pipeline/aggregator"
+	"wc/internal/cli"
+	"wc/internal/pipeline/aggregator"
 )
 
 func main() {
-    runner := cli.Runner{Engine: aggregator.DefaultEngine()}
-    exitCode := runner.Run(os.Args[1:], os.Stdin, os.Stdout, os.Stderr)
-    if exitCode != 0 {
-        os.Exit(exitCode)
-    }
+	runner := cli.Runner{Engine: aggregator.DefaultEngine()}
+	exitCode := runner.Run(os.Args[1:], os.Stdin, os.Stdout, os.Stderr)
+	if exitCode != 0 {
+		os.Exit(exitCode)
+	}
 }
